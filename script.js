69241fcc17979f0a382c94e9ee550bc2f02b9e8d
HARI BABU ANAKAPALLI <142531911+Haribabu-18@users.noreply.github.com>
2026-01-02 12:37:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll(".square");
+
+squares.forEach((square) => {
+    square.addEventListener("mouseenter", () => {
+        squares.forEach((otherSquare) => {
+            if(square !== otherSquare){
+                otherSquare.style.backgroundColor = "#6F4E37";
+            }
+        })
+    })
+})
+
+squares.forEach((square) => {
+    square.addEventListener("mouseleave", () => {
+        squares.forEach((otherSquare) => {
+            if(square !== otherSquare){
+                otherSquare.style.backgroundColor = "#E6E6FA";
+            }
+        })
+    })
+})
